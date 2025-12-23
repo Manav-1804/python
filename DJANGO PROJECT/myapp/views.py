d6280d2952a2cdf0c61dcbaffac1af83a3f9dc6a
Manav-1804 <manav.prajapati.in1808@gmail.com>
2025-12-23 12:14:54 +0530

    Python

---
+++ DJANGO PROJECT/myapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from .models import Contact
+def index(request):
+    return render(request,'index.html')
+
+def contact(request):
+    if request.method=='POST':
+        Contact.objects.create(
+            name=request.POST['name'],
+            email=request.POST['email'],
+            mobile=request.POST['mobile'],
+            remarks=request.POST['remarks'],
+        )
+        msg='Contact Added Successfully'
+        return render(request,'contact.html',{'msg':msg})
+    else:
+         return render(request,'contact.html')
+         
+   
+
+def signup(request):
+    return render(request,'signup.html')
+
+def login(request):
+    return render(request,'login.html')
